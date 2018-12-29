452bf7a8f1921063ad10c1a0f314283ff5c2a3f8
Dj0nny <frank96990@gmail.com>
2018-12-29 13:16:18 +0100

    delete useless files and setup Apollo client

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import RecordsList from './components/RecordsList/RecordsList';
+
 import './App.css';
 
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/graphql'
+});
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<ApolloProvider client={client}>
+				<div className="App">
+					<div className="main">
+						<div className="row">
+							<div className="columns medium-12 text-center">
+								<h1>React Record Shop</h1>
+							</div>
+						</div>
+						<RecordsList />
+					</div>
+				</div>
+			</ApolloProvider>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/RecordsList/RecordsList.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class RecordsList extends Component {
+	state = {};
+	render() {
+		return (
+			<div className="records-list">
+				<ul>
+					<li>Records</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default RecordsList;
