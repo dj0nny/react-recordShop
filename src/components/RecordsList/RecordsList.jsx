75e792d899a297b8075e302bdfa19a0c0fc2790d
Dj0nny <frank96990@gmail.com>
2018-12-29 13:42:22 +0100

    add records query

--- src/components/RecordsList/RecordsList.jsx
+++ src/components/RecordsList/RecordsList.jsx
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getRecordsQuery = gql`
+	{
+		records {
+			id
+			title
+			genre
+			releaseYear
+		}
+	}
+`;
 
 class RecordsList extends Component {
 	state = {};
+
+	displayRecords() {
+		var data = this.props.data;
+		if (data.loading) {
+			return <span>Loading...</span>;
+		} else {
+			return data.records.map((record) => {
+				return <li key={record.id}>{record.title}</li>;
+			});
+		}
+	}
 	render() {
+		//console.log(this.props);
 		return (
 			<div className="records-list">
-				<ul>
-					<li>Records</li>
-				</ul>
+				<ul>{this.displayRecords()}</ul>
 			</div>
 		);
 	}
 }
 
-export default RecordsList;
+export default graphql(getRecordsQuery)(RecordsList);
